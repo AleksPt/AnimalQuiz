27098c962118c1711f66d4013cad57f6ce7f3520
Alex Petrenko <alex.pt@ya.ru>
2023-11-30 17:12:34 +0300

    Initial Commit

---
+++ AnimalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalQuiz
+//
+//  Created by Алексей on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
